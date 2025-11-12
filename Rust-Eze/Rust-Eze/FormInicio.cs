30d29267f1ac9f0dff8278cc76675f24857774e0
Luca <parislucamatias@gmail.com>
2025-11-12 15:11:26 -0300

    Cambios finales

--- Rust-Eze/Rust-Eze/FormInicio.cs
+++ Rust-Eze/Rust-Eze/FormInicio.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
-
+//a
 namespace Rust_Eze
 {
     public partial class FormInicio : Form
